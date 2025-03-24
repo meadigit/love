435285d9054c3455f054cecb1e50413b3ddd2905
meadigit <lodosdigital@gmail.com>
2025-03-25 02:36:02 +0300

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Merhaba. Sana söylemem gereken bir şey var Dila. </br> Benimle çıkar mısın?",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "Seçimi tamamen sana bırakıyorum. Seçimlerinde özgürsün.",
+  buttonYes: "EVET",
+  buttonNo: "HAYIR",
+  titleModar: "Harika!",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Tamamen özgür iradenle bu tercihi yaptın. Sonuçlarından sorumlu değilim Dila!</br> Seni seviyorum <3",
 };
