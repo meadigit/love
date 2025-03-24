d407fb0f36a46da8f4e1f39c61aeefa5a67dac98
meadigit <lodosdigital@gmail.com>
2025-03-25 02:37:38 +0300

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -2,8 +2,8 @@ const CONFIGDATA = {
   titleHeader: "Merhaba. Sana söylemem gereken bir şey var Dila. </br> Benimle çıkar mısın?",
   descriptionHeader:
     "Seçimi tamamen sana bırakıyorum. Seçimlerinde özgürsün.",
-  buttonYes: "EVET",
-  buttonNo: "HAYIR",
+  buttonYes: "Evet",
+  buttonNo: "Hayır",
   titleModar: "Harika!",
   descriptionModar:
     "Tamamen özgür iradenle bu tercihi yaptın. Sonuçlarından sorumlu değilim Dila!</br> Seni seviyorum <3",
